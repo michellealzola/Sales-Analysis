f69df6dd8db36ebd3f8436922382136cc043ca3b
Michelle Alzola <105762453+michellealzola@users.noreply.github.com>
2023-04-26 18:31:54 -0600

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace Sales_Analysis
+{
+    partial class frmSalesAnalysis
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.lboxSalesList = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblAverage = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblHighest = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.lblLowest = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arsenal", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(102, 26);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "List of Sales";
+            // 
+            // lboxSalesList
+            // 
+            this.lboxSalesList.Font = new System.Drawing.Font("Arsenal", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lboxSalesList.FormattingEnabled = true;
+            this.lboxSalesList.ItemHeight = 26;
+            this.lboxSalesList.Location = new System.Drawing.Point(12, 38);
+            this.lboxSalesList.Name = "lboxSalesList";
+            this.lboxSalesList.Size = new System.Drawing.Size(387, 160);
+            this.lboxSalesList.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arsenal", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(415, 46);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(112, 26);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Average Sale:";
+            // 
+            // lblAverage
+            // 
+            this.lblAverage.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblAverage.Font = new System.Drawing.Font("Arsenal", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAverage.Location = new System.Drawing.Point(533, 38);
+            this.lblAverage.Name = "lblAverage";
+            this.lblAverage.Size = new System.Drawing.Size(255, 40);
+            this.lblAverage.TabIndex = 3;
+            this.lblAverage.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Arsenal", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(417, 98);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(110, 26);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Highest Sale:";
+            // 
+            // lblHighest
+            // 
+            this.lblHighest.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblHighest.Font = new System.Drawing.Font("Arsenal", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHighest.Location = new System.Drawing.Point(533, 90);
+            this.lblHighest.Name = "lblHighest";
+            this.lblHighest.Size = new System.Drawing.Size(255, 40);
+            this.lblHighest.TabIndex = 5;
+            this.lblHighest.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Arsenal", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(421, 149);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(106, 26);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "Lowest Sale:";
+            // 
+            // lblLowest
+            // 
+            this.lblLowest.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblLowest.Font = new System.Drawing.Font("Arsenal", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLowest.Location = new System.Drawing.Point(533, 141);
+            this.lblLowest.Name = "lblLowest";
+            this.lblLowest.Size = new System.Drawing.Size(255, 40);
+            this.lblLowest.TabIndex = 7;
+            this.lblLowest.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Arsenal", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(12, 220);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(387, 53);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Get Values";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Arsenal", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(405, 220);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(229, 53);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "Clear";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Arsenal", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(640, 220);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(148, 53);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "Exit";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // frmSalesAnalysis
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 291);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lblLowest);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.lblHighest);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lblAverage);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lboxSalesList);
+            this.Controls.Add(this.label1);
+            this.Name = "frmSalesAnalysis";
+            this.Text = "Sales Analysis";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox lboxSalesList;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblAverage;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblHighest;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label lblLowest;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sales_Analysis
+{
+    public partial class frmSalesAnalysis : Form
+    {
+        public frmSalesAnalysis()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                StreamReader inputFile = File.OpenText("Sales.txt");
+
+                List<string> sales = new List<string>();
+
+                while(!inputFile.EndOfStream)
+                {
+                    sales.Add(inputFile.ReadLine());
+                }
+
+                inputFile.Close();
+
+                foreach(string sale in sales)
+                {
+                    lboxSalesList.Items.Add(sale);
+                }
+
+                double average = GetAverageSale(sales);
+                double highest = GetHighestSale(sales);
+                double lowest = GetLowestSale(sales);
+
+                lblAverage.Text = average.ToString("c");
+                lblHighest.Text = highest.ToString("c");
+                lblLowest.Text = lowest.ToString("c");
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private double GetLowestSale(List<string> sales)
+        {
+            double lowestSale = double.Parse(sales[0]);
+
+            foreach(string sale in sales)
+            {
+                if(lowestSale > double.Parse(sale))
+                {
+                    lowestSale = double.Parse(sale);
+                }
+            }
+
+            return lowestSale;
+        }
+
+        private double GetHighestSale(List<string> sales)
+        {
+            double higestSale = double.Parse(sales[0]);
+
+            foreach (string sale in sales)
+            {
+                if (higestSale < double.Parse(sale))
+                {
+                    higestSale = double.Parse(sale);
+                }
+            }
+
+            return higestSale;
+        }
+
+        private double GetAverageSale(List<string> sales)
+        {
+            double total = 0;
+
+            foreach(string sale in sales)
+            {
+                total += double.Parse(sale);
+            }
+
+            double average = total / sales.Count;
+
+            return average;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            lboxSalesList.Items.Clear();
+            lblAverage.Text = "";
+            lblHighest.Text = "";
+            lblLowest.Text = "";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
